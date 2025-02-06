614b8c2c3f5fd6c935fe7a8ef5a94f9b673dec94
Jakub Sido <JakubSido@gmail.com>
2025-02-06 09:41:19 +0100

    init

---
+++ 01-salary/main.py
@@ -0,0 +1,29 @@
+
+def load_data_file(filepath: str, wanted_year: int):
+    people : list[Person] = list()
+    return people
+
+
+def count_average_salary(data: list[Person]):
+    return 0                    
+
+
+def count_median_salary(data: list[Person]):
+    return 0                
+
+
+def main(input_path: str):
+
+    year = None         
+    data = None         
+    avg = count_average_salary(data)
+    print(f"Průměrný plat v roce {year}  je  {avg}")
+
+    median = count_median_salary(data)
+    print(f"Medián v roce {year}  je  {median}")
+
+
+if __name__ == '__main__':
+    data_path = None
+    main(data_path)
+
